d0eadb6af5dd5e11c5c32a6b7e171d7220e4161a
Amanyyy27 <116085100+Amanyyy27@users.noreply.github.com>
2025-03-23 22:45:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
 import { Navbar2 } from './Components/Navbar/Navbar2';
 import Footer from './Components/Footer/Footer';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import Shop from './Pages/Shop';
 import Home from './Pages/Home';
 import Cart from './Pages/Cart';
@@ -12,35 +12,47 @@ import LoginSignup from './Pages/LoginSignup';
 import ShopCategory from './Pages/ShopCategory';
 import About from './Pages/About';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // State to track login
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleLogin = () => {
-    setIsLoggedIn(true); // Update login state when the user logs in
+    setIsLoggedIn(true);
   };
 
   return (
     <div>
       <BrowserRouter>
-        {isLoggedIn ? <Navbar2 /> : <Navbar />} {/* Switch navbar */}
+        <ScrollToTop />
+        {isLoggedIn ? <Navbar2 /> : <Navbar />}
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/shop' element={<Shop />}>
-            <Route path=':productId' element={<Shop />} />
-          </Route>
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/favourites' element={<Favourites />} />
-          <Route path='/eye care' element={<ShopCategory category='eye-care' />} />
-          <Route path='/lip care' element={<ShopCategory category='lip-care' />} />
-          <Route path='/cleansing' element={<ShopCategory category='cleansing' />} />
-          <Route path='/exfoliating' element={<ShopCategory category='exfoliating' />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/login-signup' element={<LoginSignup onLogin={handleLogin} />} /> 
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/shop/:productId" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/favourites" element={<Favourites />} />
+          <Route path="/eye care" element={<ShopCategory category="eye-care" />} />
+          <Route path="/lip care" element={<ShopCategory category="lip-care" />} />
+          <Route path="/cleansing" element={<ShopCategory category="cleansing" />} />
+          <Route path="/exfoliating" element={<ShopCategory category="exfoliating" />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login-signup" element={<LoginSignup onLogin={handleLogin} />} />
+          <Route path="*" element={<Navigate to="/" replace />} /> {/* Redirect unknown URLs to Home */}
         </Routes>
-        <Footer></Footer>
+        <Footer />
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
